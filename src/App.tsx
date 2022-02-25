866759b2af84b1bf86afff4d0fcf685bd08174bb
kis0421 <kig0421@gmail.com>
2022-02-25 20:35:11 +0900

    일단 움직이게

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,36 @@
-import * as React from "react";
-const Sheep = () => {
-  return <pre style={{
-    fontSize: 4,
-    transform: "scaleX(-1)",
-    display: "inline-block"
-  }}> {`
- ⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒
+import React, { useEffect, useState } from "react";
+const Sheep = (props: { positionX: number, positionY: number, direction: "right" | "left" }) => {
+  const { positionX, positionY, direction } = props;
+  return <pre
+    id="sheep"
+    style={{
+      left: positionX,
+      top: positionY,
+      transform: direction === "right" ? "scaleX(-1)" : "scaleX(1)",
+    }}> {`⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒
 ⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒
 Ꮚ･ꈊ･Ꮚ ⌒⌒⌒⌒⌒⌒⌒⌒3
 ⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒
 ⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒⌒
-∏ ∏         ∏ ∏
-
-  `}</pre >
+∏ ∏         ∏ ∏`}</pre >
 }
 const App = () => {
+  const [x, setX] = useState(0)
+  const [y, setY] = useState(0)
+  const [dir, setDir] = useState("right")
+
+  const setMoveEventHandler = (e: any) => {
+    if (e.clientX > x) {
+      setDir("right")
+    } else {
+      setDir("left")
+    }
+    setX(e.clientX)
+    setY(e.clientY)
+  }
+  window.addEventListener("click", setMoveEventHandler);
   return <>
-    <Sheep />
+    <Sheep positionX={x} positionY={y} direction={dir as "left" | "right"} />
   </>;
 }
 export default App;
